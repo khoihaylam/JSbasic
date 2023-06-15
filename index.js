7d2afa338188b628fa56df5321000391ef5ef668
khoihaylam <130764388+khoihaylam@users.noreply.github.com>
2023-06-15 21:06:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//bài 1
+console.log("xin chào rikkei academy")
+//bài 3
+document.write("quyết tâm học lập trình javascrit")
+//bài 4
+alert("chào mừng bạn đến với học viện rikkei academy")
+//bài 5
+confirm("bạn đã đủ 18 tuổi")
